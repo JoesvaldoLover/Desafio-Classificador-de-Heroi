ca16a0af95a728539c7b645446f5c27630c60b9f
lucas gomes <theminimare4514@gmail.com>
2024-10-25 09:54:38 -0300

    Criacao de funcao para classificar nivel

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+function getHeroiNivel(XP) {
+    let nivel = "Ferro"
+    
+    if(XP > 1000 && XP <= 2000)
+        nivel = "Bronze"
+    else if(XP > 2000 && XP <= 5000)
+        nivel = "Prata"
+    else if(XP > 5000 && XP <= 7000)
+        nivel = "Ouro"
+    else if(XP > 7000 && XP <= 8000)
+        nivel = "Platina"
+    else if(XP > 8000 && XP <= 9000)
+        nivel = "Ascendente"
+    else if(XP > 9000 && XP <= 10000)
+        nivel = "Imortal"
+    else if(XP > 10000)
+        nivel = "Radiante"
+
+    return nivel
+}
+
 // Variável XP
 const heroiNome = "Felipe"
-let heroiXP = 1000
-let heroiNivel = "Ferro"
+let heroiXP = 5246
 
-if(heroiXP > 1000 && heroiXP <= 2000)
-    heroiNivel = "Bronze"
-else if(heroiXP > 2000 && heroiXP <= 5000)
-    heroiNivel = "Prata"
-else if(heroiXP > 5000 && heroiXP <= 7000)
-    heroiNivel = "Ouro"
-else if(heroiXP > 7000 && heroiXP <= 8000)
-    heroiNivel = "Platina"
-else if(heroiXP > 8000 && heroiXP <= 9000)
-    heroiNivel = "Ascendente"
-else if(heroiXP > 9000 && heroiXP <= 10000)
-    heroiNivel = "Imortal"
-else if(heroiXP > 10000)
-    heroiNivel = "Radiante"
+
+let heroiNivel = getHeroiNivel(heroiXP)
 
 console.log("O Herói de nome " + heroiNome + " está no nível de " + heroiNivel)
 
